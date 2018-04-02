1d90057d7d431597cf066e2ef21086bc81abb59f
lemonfcm <547169149@qq.com>
2018-04-02 14:59:38 +0800

    卡牌PC端完成了一部分

---
+++ kakp_pc/src/js/app/index.js
@@ -0,0 +1,51 @@
+nie.define("Index",function(){
+    'use strict';
+     var Controller = {
+
+       initShare:function(){
+            var shareV5 = nie.require("nie.util.shareV5");
+          /*  var shareUrl = $("#share_url").html();*/
+            var sharePic = $("#share_pic").attr("data-src");
+            var shareTxt = $("#share_desc").html();
+            var shareTitle = $("#share_title").html();
+            var share = shareV5({
+                fat: "#NIE-share", 
+                type: 6, 
+                defShow: [23, 22, 2, 1,4], 
+                title: shareTitle,  
+                img: sharePic, 
+                content: shareTxt 
+            });
+        },
+
+        initScroll:function(){
+        	$(window).scroll(function(){
+                if ($(window).scrollTop()>100){
+                    $("#backToTop").fadeIn(1000);
+                }
+                else
+                {
+                    $("#backToTop").fadeOut(1000);
+                }
+            });
+    	    $("#backToTop").click(function(){
+           		$('body,html').animate({scrollTop:0},1000);
+        			      
+     	    }); 
+    	},
+    	initNiceScroll:function(){
+			$("#skzsNiceScroll").niceScroll({cursorcolor:"#4f3c13"});
+    	},
+
+    	init:function(){
+			this.initShare();
+			this.initScroll();
+			this.initNiceScroll();
+    	}
+  
+
+    };
+
+    Controller.init();
+
+});
